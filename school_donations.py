d841a547f92a0f5ff88fce1ca774088870cd9c14
Frank Mullen <frankmullen@gmail.com>
2017-02-12 11:43:53 +0000

    deleted passwords

--- school_donations.py
+++ school_donations.py
@@ -11,10 +11,7 @@ DBS_NAME = os.environ.get('DBS_NAME')
 MONGO_URI = os.environ.get('MONGO_URI')
 MONGODB_HOST = os.environ.get('MONGODB_HOST')
 MONGODB_PORT = os.environ.get('MONGODB_PORT')
-#MONGODB_HOST = 'ds149069.mlab.com'
-#MONGODB_PORT = 49069
-#DBS_NAME = 'heroku_sl7272bv'
-#MONGO_URI = 'mongodb://root:schoolheroku@ds149069.mlab.com:49069/heroku_sl7272bv'
+
 COLLECTION_NAME = 'opendata_projects_clean'
 FIELDS = {'funding_status': True, 'school_state': True, 'resource_type': True, 'poverty_level': True,
           'date_posted': True, 'total_donations': True, '_id': False}
